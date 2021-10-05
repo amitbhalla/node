add7744ce4e546ff7edd495876dd82a07bfd9cb1
amitbhalla <amit9815@gmail.com>
2021-10-05 16:31:04 +0530

    Promise chain

--- index.js
+++ index.js
@@ -1,10 +1,44 @@
-const http = require('http');
+const fs = require('fs');
+const superagent = require('superagent');
 
-const server = http.createServer((req, res) => {
-    console.log(req);
-    res.end('Hello World!');
-});
+const readFilePro = (file) => {
+    return new Promise((resolve, reject) => {
+        fs.readFile(file, (err, data) => {
+            if (err) {
+                reject('Error accessing the file!');
+            } else {
+                resolve(data);
+            }
+        });
+    });
+};
 
-server.listen(3000, '0.0.0.0', (err) => {
-    console.log('listening to 3000 on 0.0.0.0');
-});
+const writeFilePro = (file, data) => {
+    return new Promise((resolve, reject) => {
+        fs.writeFile(file, data, (err, data) => {
+            if (err) {
+                reject('Error accessing the file!');
+            } else {
+                resolve(data);
+            }
+        });
+    });
+};
+
+readFilePro(`${__dirname}/dog.txt`)
+    .then((data) => {
+        console.log(`Breed: ${data}`);
+        return superagent.get(
+            `https://dog.ceo/api/breed/${data}/images/random`
+        );
+    })
+    .then((res) => {
+        return writeFilePro('dog-img.txt', res.body.message);
+        console.log(res.body.message);
+    })
+    .then(() => {
+        console.log('Random Dog image saved!');
+    })
+    .catch((err) => {
+        console.log(err.message);
+    });
