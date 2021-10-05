ed019a24e2368d38fb23806ea1d2be63a6f3765b
amitbhalla <amit9815@gmail.com>
2021-10-05 12:54:46 +0530

    Callback hell

---
+++ index.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+const superagent = require('superagent');
+
+fs.readFile(`${__dirname}/dog.txt`, 'utf-8', (err, data) => {
+    superagent
+        .get(`https://dog.ceo/api/breed/${data}/images/random`)
+        .end((err, res) => {
+            console.log(res.body.message);
+            fs.writeFile('dog-img.txt', res.body.message, (err) => {
+                console.log('Image URL saved');
+            });
+        });
+});
